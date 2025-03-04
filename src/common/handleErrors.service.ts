cb8911d0d3eb1b6b000469fef1d5324adb0d6124
navidul hoque <hnavidul@gmail.com>
2025-03-04 12:26:59 +0600

    api

--- src/common/handleErrors.service.ts
+++ src/common/handleErrors.service.ts
@@ -4,18 +4,30 @@ import { BadRequestException, ForbiddenException, Injectable, NotFoundException,
 export class HandleErrorsService {
 
     throwNotFoundError(message: string) {
-        throw new NotFoundException(message)
+        throw new NotFoundException(message, {
+            cause: new Error(),
+            description: 'Resources not found',
+        })
     }
 
     throwBadRequestError(message: string) {
-        throw new BadRequestException(message);
+        throw new BadRequestException(message, {
+            cause: new Error(),
+            description: 'Your requested data is not valid',
+        });
     }
-    
+
     throwUnauthorizedError(message: string) {
-        throw new UnauthorizedException(message);
+        throw new UnauthorizedException(message, {
+            cause: new Error(),
+            description: 'Your certain action is unauthorized',
+        });
     }
-    
+
     throwForbiddenError(message: string) {
-        throw new ForbiddenException(message);
+        throw new ForbiddenException(message, {
+            cause: new Error(),
+            description: 'Your certain action is forbidden',
+        });
     }
 }

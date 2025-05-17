d16f937d04d6ed9c008f3c828bf0e22c806c47a2
Md.Navidul Hoque <hnavidul@gmail.com>
2025-05-17 14:27:12 +0600

    feat: implement review creation functionality with validation and error handling

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common';
+import { ReviewService } from './review.service';
+import { ReviewDto } from './dto';
 
 @Controller('review')
-export class ReviewController {}
+export class ReviewController {
+
+    constructor(
+        private readonly reviewService: ReviewService
+    ) { }
+
+    @Post("/create-review")
+    async createReview(@Body() reviewDto: ReviewDto) {
+        return this.reviewService.createReview(reviewDto);
+    }
+}
+
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ReviewDto } from './dto';
+import { HandleErrorsService } from 'src/common/handleErrors.service';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+    constructor(
+        private prisma: PrismaService,
+        private handleErrorsService: HandleErrorsService
+    ) { }
+
+    async createReview(dto: ReviewDto) {
+
+        const { patientId, doctorId, comment, rating } = dto;
+
+        try {
+            const review = await this.prisma.review.create({
+                data: {
+                    patientId,
+                    doctorId,
+                    comment,
+                    rating
+                }
+            })
+
+            return {
+                data: review,
+                message: "Review created successfully"
+            }
+        }
+
+        catch (error) {
+            this.handleErrorsService.handleError(error)
+        }
+    }
+}
